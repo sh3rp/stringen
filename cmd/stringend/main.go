f05713681d8ce4bd22c5071e9316b9a36fb51e1a
Shepherd Kendall <s@kndl.io>
2022-08-01 21:12:44 -0400

    added configurable port number

--- cmd/stringend/main.go
+++ cmd/stringend/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	log2 "github.com/sh3rp/stringen/pkg/log"
 	"github.com/sh3rp/stringen/pkg/server"
 	"log"
 	"net/http"
 )
 
+var port int
+
 func main() {
 	log2.LOGGER.Info().Msgf("StrinGen v1.0")
+	flag.IntVar(&port, "p", 8888, "Port to run on")
 	svc := server.NewService(http.NewServeMux())
-	log.Fatal(svc.Serve(":8888"))
+	log.Fatal(svc.Serve(fmt.Sprintf(":%d", port)))
 }
